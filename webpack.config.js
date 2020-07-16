c30e9ab43027c64f8e4c96e43f31277296b0b474
Bertil Tandayamo <bertilt4@gmail.com>
2020-07-16 16:33:19 -0500

    Add devserver config - Add port 9000 and content base from dist folder - Clean all nt usefull html on dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
+const { CleanWebPackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  plugins: [new CleanWebPackPlugin({ CleanWebPackPlugin: false })],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 900,
   },
 };
