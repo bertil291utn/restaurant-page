0a011d4043fa7a49d2fda3cc41f90140e428a0b4
Bertil Tandayamo <bertilt4@gmail.com>
2020-07-16 16:21:40 -0500

    Webpack config - Output and devtool inline source config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
