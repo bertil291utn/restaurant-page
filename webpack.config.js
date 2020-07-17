0fe3b2c1f94f1c2f1b748fbc5214e2e0ed17c24d
Bertil Tandayamo <bertilt4@gmail.com>
2020-07-17 11:19:33 -0500

    Webpack config - Add css and fie loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
