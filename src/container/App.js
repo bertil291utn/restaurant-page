26e6285ceb70a488ab6595d2c2c7cbc26b5c9bd7
Bertil Tandayamo <bertilt4@gmail.com>
2020-07-18 19:22:51 -0500

    Add home page - Cresate a new page to add to index file

--- src/container/App.js
+++ src/container/App.js
@@ -1,14 +1,20 @@
-import createElementContainer from '../elements/element_container';
-import Header from '../components/header';
 import 'css-reset-and-normalize';
 import '../styles/global.css';
+import createElementContainer from '../elements/element_container';
+import Header from '../components/header';
+import Home from '../pages/home';
 
 const App = () => {
   const bodyContainerElement = createElementContainer('div', [
     'body-container',
   ]);
   const navBar = Header();
-  bodyContainerElement.append(navBar);
+  const main = createElementContainer('main');
+  // check with switch to change the main page
+  // first check with hide and show 
+  // then could try deleting all inner element inerhtml=''
+  main.append(Home());
+  bodyContainerElement.append(navBar, main);
   return bodyContainerElement;
 };
 
---
+++ src/pages/home.js
@@ -0,0 +1,38 @@
+import createElementContainer from '../elements/element_container';
+import imgHero from '../assets/sushi-roll_hero-section.png';
+
+const Home = () => {
+  const section = createElementContainer('section', ['hero']);
+  const heroContent = createElementContainer('div', ['hero-content']);
+  section.append(heroContent);
+  const fadingAnimation = createElementContainer('div', [
+    'slide-fading-animation',
+  ]);
+  heroContent.append(fadingAnimation);
+  const paragraphHero = createElementContainer('p');
+  paragraphHero.innerHTML = 'japanese tradition and the best ingredients with modern touches';
+  fadingAnimation.append(paragraphHero);
+  const heroButtons = createElementContainer('div', ['hero-buttons']);
+  const exploreButton = createElementContainer(
+    'button',
+    ['primary-button'],
+    'explore-button',
+  );
+  exploreButton.innerHTML = 'Explore';
+  heroButtons.append(exploreButton);
+  const menuButton = createElementContainer('button');
+  menuButton.innerHTML = 'Menu';
+  heroButtons.append(menuButton);
+  fadingAnimation.append(heroButtons);
+  const imgHeroElement = createElementContainer(
+    'img',
+    ['slide-fading-shorter-animation'],
+    'sushi-hero',
+  );
+  imgHeroElement.src = imgHero;
+  imgHeroElement.alt = 'sushi hero section';
+  heroContent.append(imgHeroElement);
+  return section;
+};
+
+export default Home;
